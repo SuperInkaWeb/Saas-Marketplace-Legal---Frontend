709fb868df96d9f433cced714532fafd2344d4ca
Gabriel <i202332157@cibertec.edu.pe>
2026-04-27 12:42:31 -0500

    clean

--- src/modules/auth/hooks.ts
+++ src/modules/auth/hooks.ts
@@ -14,6 +14,7 @@ import type {
   CreateLawyerProfileRequest,
   UploadIdentityDocumentRequest
 } from "./types";
+import { toast } from "sonner";
 import { AxiosError } from "axios";
 import { ApiErrorResponse } from "@/utils/types";
 
